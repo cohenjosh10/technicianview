557fdde25f45cde8fd7f1926ffbf3513a5361ade
Josh Cohen <jhcohen@fb.com>
2021-05-01 01:16:42 -0400

    [technicianview] Add 'api/v1/solar_farms/<solar_farm_id>/technicians api
    Summary:
    Expose the basic api for solar farm technicians, and print out a basic
    message confirming we've received the request.
    
    Test Plan:
    In browser navigate to:
      http://127.0.0.1:5000/api/v1/solar_farms/abc123/technicians
    
    Ensure you see the message:
      Received request for farm with id abc123

--- backend/TechnicianApi.py
+++ backend/TechnicianApi.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return 'Index Page'
+
+@app.route('/api/v1/solar_farms/<solar_farm_id>/technicians',
+        methods=['GET'])
+def technician_location(solar_farm_id):
+  return 'Received request for farm with id ' + solar_farm_id
