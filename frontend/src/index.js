f585f4fb165c64c82c58739f25015876b072335b
Josh Cohen <jhcohen@fb.com>
2021-05-03 01:29:49 -0400

    [technicianview] Add marker to screen
    Summary:
    Add a marker to map with specified coordinates, color, rotation, and
    popup title. Uses these examples:
    * https://docs.mapbox.com/mapbox-gl-js/example/add-a-marker/
    * https://docs.mapbox.com/mapbox-gl-js/api/markers/
    
    Test Plan:
    Ensure when you open your map, you see a marker at a rotation and random
    color in the middle of the screen.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,11 +20,24 @@ const Map = () => {
       center: [lng, lat],
       zoom: zoom
     });
+
     map.on('move', () => {
       setLng(map.getCenter().lng.toFixed(4));
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
     });
+
+    var marker1Lng = -70.9;
+    var marker1Lat = 42.35;
+    var marker1Color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    var marker1Title = "Technician 1"
+    var marker1Rotation = 90;
+    var marker1 = new mapboxgl.Marker({color: marker1Color})
+      .setLngLat([marker1Lng, marker1Lat])
+      .setPopup(new mapboxgl.Popup().setText(marker1Title))
+      .setRotation(marker1Rotation)
+      .addTo(map);
+
     return () => map.remove();
   }, []);
 
