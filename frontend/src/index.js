98460e66a191b8f985637bea7f8f529633bfccb3
Josh Cohen <jhcohen@fb.com>
2021-05-04 02:39:14 -0400

    [technicianview] Parse response to display technician list
    Summary:
    Parse the response to display all technicians on the map.
    
    Test Plan:
    Load the react app, and observe you can view the three coordinates on
    the map in calexico.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,28 +7,36 @@ import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = 'pk.eyJ1IjoiY29oZW5qb3NoMTAiLCJhIjoiY2tvODM3cHViMWh5MDJ3bWxlODR5OHJwbyJ9.OsP4XQxhznEl3LkexbZyNg';
 const TECHNICIANS_API = 'http://127.0.0.1:5000/api/v1/solar_farms/abc123/technicians';
-
+const GREENTOWN_LABS_LNG = -71.102768;
+const GREENTOWN_LABS_LAT = 42.381729;
 
 const Map = () => {
   const mapContainer = useRef();
-
-  // Technician Information
-  const [techLng, setTechLng] = useState(0);
-  const [techLat, setTechLat] = useState(0);
-  const [techTitle, setTechTitle] = useState("");
-  const [techRot, setTechRot] = useState(0);
-  const [techColor, setTechColor] = useState("#" + Math.floor(Math.random()*16777215).toString(16));
+  const [techDict, setTechDict] = useState(null);
 
   // Technician Fetching
   const fetchTechnicianLocation = () => {
     return fetch(TECHNICIANS_API)
       .then((response) => response.json())
       .then((json) => {
-        const tech = json.features[0];
-        setTechLng(tech.geometry.coordinates[0]);
-        setTechLat(tech.geometry.coordinates[1]);
-        setTechTitle(tech.properties.name);
-        setTechRot(tech.properties.bearing);
+        const newTechDict = {};
+        var isNewSnapshot = false;
+        for (const tech of json.features) {
+          const name = tech.properties.name;
+          if (techDict === null || techDict[name] === null) {
+            isNewSnapshot = true;
+            tech.properties.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+          } else {
+            if (tech.properties.tsecs != techDict[name].properties.tsecs) {
+              isNewSnapshot = true;
+            }
+            tech.properties.color = techDict[name].properties.color;
+          }
+          newTechDict[name] = tech;
+        }
+        if (isNewSnapshot) {
+          setTechDict(newTechDict);
+        }
       })
       .catch((error) => {
         console.error(error);
@@ -38,18 +46,33 @@ const Map = () => {
   useEffect(() => {
     fetchTechnicianLocation();
 
+    const techs = techDict != null ? Object.values(techDict) : [];
+
+    // Set screen center to the average of the existing technicians. If no
+    // technicians are provided, set screen center to the Greentown Labs
+    var latSum = 0;
+    var lngSum = 0;
+    for (const tech of techs) {
+      lngSum += tech.geometry.coordinates[0];
+      latSum += tech.geometry.coordinates[1];
+    }
+    const centerLng = techs.length > 0 ? lngSum / techs.length : GREENTOWN_LABS_LNG;
+    const centerLat = techs.length > 0 ? latSum / techs.length : GREENTOWN_LABS_LAT;
+
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [techLng, techLat],
-      zoom: 9
+      center: [centerLng, centerLat],
+      zoom: 12
     });
 
-    var marker1 = new mapboxgl.Marker({color: techColor})
-      .setLngLat([techLng, techLat])
-      .setPopup(new mapboxgl.Popup().setText(techTitle))
-      .setRotation(techRot)
-      .addTo(map);
+    for (const tech of techs) {
+      const marker = new mapboxgl.Marker({color: tech.properties.color})
+        .setLngLat([tech.geometry.coordinates[0], tech.geometry.coordinates[1]])
+        .setPopup(new mapboxgl.Popup().setText(tech.properties.name))
+        .setRotation(tech.properties.bearing)
+        .addTo(map);
+    }
 
     return () => map.remove();
   });
