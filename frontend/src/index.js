769716ac12090ae8847a5cca39520996c31bc6e7
Josh Cohen <jhcohen@fb.com>
2021-05-04 05:53:13 -0400

    [technicianview] Add a notification when pins get close together
    Summary:
    When technicians get close, we want to post a notif indicating that they
    are close. Product behavior provided is: we show one notification for
    every time one technician enters within a 300M radius of another.
    
    Test Plan:
    Ensure notification shows at the end of the demo.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
 import React, { useRef, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import {toast} from 'react-toastify';
+import haversine from 'haversine-distance';
+import 'react-toastify/dist/ReactToastify.css';
 
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = 'pk.eyJ1IjoiY29oZW5qb3NoMTAiLCJhIjoiY2tvODM3cHViMWh5MDJ3bWxlODR5OHJwbyJ9.OsP4XQxhznEl3LkexbZyNg';
-const TECHNICIANS_API = 'http://127.0.0.1:5000/api/v1/solar_farms/abc123/technicians';
 const GREENTOWN_LABS_LNG = -71.102768;
 const GREENTOWN_LABS_LAT = 42.381729;
 const SERVER_REFRESH_MS = 1000;
+const CLOSE_PROXIMITY_M = 304.8
 
+const TECHNICIANS_API = 'http://127.0.0.1:5000/api/v1/solar_farms/abc123/technicians';
 const APPLICATION_START_TIME_MS = new Date().getTime();
 const MOCK_DATA_START_TIME_S = 1592078400;
 const MOCK_DATA_SIMULATION_SPEED = 20;
@@ -19,6 +23,9 @@ const MS_IN_S = 1000;
 const mTechnicianData = {};
 const mMarkers = {};
 var mMap;
+const mCloseTechnicianPairs = new Set();
+
+toast.configure();
 
 const getMockedTime = () => {
   return MOCK_DATA_START_TIME_S + Math.round((new Date().getTime() - APPLICATION_START_TIME_MS) / MS_IN_S) * MOCK_DATA_SIMULATION_SPEED;
@@ -34,25 +41,30 @@ const Map = () => {
     return fetch(TECHNICIANS_API + "/" + mockedTime)
       .then((response) => response.json())
       .then((json) => {
-        var tsecs;
-        for (const tech of json.features) {
-          const name = tech.properties.name;
-          if (mTechnicianData[name] == null) {
-            tech.properties.color = "#" + Math.floor(Math.random()*16777215).toString(16);
-            mTechnicianData[name] = tech;
-          } else {
-            tech.properties.color = mTechnicianData[name].properties.color;
-            mTechnicianData[name] = tech;
-          }
-          tsecs = tech.properties.tsecs;
-        }
-        setTsecs(tsecs);
-        scheduleTechnicianFetch();
+        parseTechnicianData(json);
       })
       .catch((error) => {
         console.error(error);
       });
     };
+
+  const parseTechnicianData = (json) => {
+    var tsecs;
+    for (const tech of json.features) {
+      const name = tech.properties.name;
+      if (mTechnicianData[name] == null) {
+        tech.properties.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+        mTechnicianData[name] = tech;
+      } else {
+        tech.properties.color = mTechnicianData[name].properties.color;
+        mTechnicianData[name] = tech;
+      }
+      tsecs = tech.properties.tsecs;
+    }
+    setTsecs(tsecs);
+    notifyIfTechniciansAreClose(tsecs);
+    scheduleTechnicianFetch();
+  }
 
   const scheduleTechnicianFetch = () => {
     setTimeout(() => {
@@ -60,13 +72,51 @@ const Map = () => {
     }, SERVER_REFRESH_MS);
   }
 
+  const notifyIfTechniciansAreClose = (tsecs) => {
+    const technicianPairs = generateTechnicianPairs();
+    for (const technicianPair of technicianPairs) {
+      const id = getTechnicianPairUniqueId(technicianPair);
+      const technicianOne = technicianPair[0];
+      const technicianTwo = technicianPair[1];
+      const technicianOneCoords = { 'latitude' : technicianOne.geometry.coordinates[1], 'longitude' : technicianOne.geometry.coordinates[0] }
+      const technicianTwoCoords = { 'latitude' : technicianTwo.geometry.coordinates[1], 'longitude' : technicianTwo.geometry.coordinates[0] }
+
+      if (haversine(technicianOneCoords, technicianTwoCoords) < CLOSE_PROXIMITY_M) {
+        if (!mCloseTechnicianPairs.has(id)) {
+          toast('[ALERT] ' + technicianOne.properties.name + ' is within 1000 feet of ' + technicianTwo.properties.name + '!');
+          mCloseTechnicianPairs.add(id);
+        } 
+      } else {
+        if (mCloseTechnicianPairs.has(id)) {
+          mCloseTechnicianPairs.add(id);
+        }
+      }
+    }
+  }
+
+  const generateTechnicianPairs = () => {
+    const technicians = Object.values(mTechnicianData);
+    const technicianPairs = [];
+    for (var i = 0; i < technicians.length; i++) {
+      for (var j = i+1; j < technicians.length; j++) {
+        technicianPairs.push([technicians[i], technicians[j]]);
+      }
+    }
+    return technicianPairs;
+  }
+
+  const getTechnicianPairUniqueId = (technicianPair) => {
+    return technicianPair[0].properties.name + technicianPair[1].properties.name;
+  }
+
   fetchTechnicianLocation();
+
 
   useEffect(() => {
     const techs = Object.values(mTechnicianData);
 
-    // Set screen center to the average of the existing technicians. If no
-    // technicians are provided, set screen center to the Greentown Labs
+    // Calculate a screen center to be the average of the existing technicians. 
+    // If no technicians are provided, set screen center to the Greentown Labs
     var latSum = 0;
     var lngSum = 0;
     for (const tech of techs) {
@@ -76,6 +126,7 @@ const Map = () => {
     const centerLng = techs.length > 0 ? lngSum / techs.length : GREENTOWN_LABS_LNG;
     const centerLat = techs.length > 0 ? latSum / techs.length : GREENTOWN_LABS_LAT;
 
+    // Render the map
     if (mMap == null) {
       mMap = new mapboxgl.Map({
         container: mapContainer.current,
@@ -87,6 +138,7 @@ const Map = () => {
       mMap.setCenter([centerLng, centerLat]);
     }
 
+    // Render the map markers
     for (const tech of techs) {
       var name = tech.properties.name;
       if (mMarkers[name] == null) {
